9b28b37a1aeb821e121d72f759b1aa61ea76978f
erik <elumpkin@caisgroup.com>
2015-09-07 23:05:44 -0400

    updating HelloWorld (stash/pull/stash-pop/merge)

--- playground/src/com/cais/playground/HelloWorld.java
+++ playground/src/com/cais/playground/HelloWorld.java
@@ -6,5 +6,6 @@ public class HelloWorld {
 		
 		System.out.println("hello world");
 		System.out.println("added with 4th commit from Eclipse (no merge necessary)");
+		System.out.println("added with 5 commit from command line (stash/pull/stash-pop)");
 	}
 }
