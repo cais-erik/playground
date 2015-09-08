58ca4120ad2cab68e7707a38e958b4eab60601e1
elumpkin <elumpkin@caisgroup.com>
2015-09-07 22:59:29 -0400

    adding addt'l sysout.  No merge necessary. (from eclipse)

--- playground/src/com/cais/playground/HelloWorld.java
+++ playground/src/com/cais/playground/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		
 		System.out.println("hello world");
+		System.out.println("added with 4th commit from Eclipse (no merge necessary)");
 	}
 }
