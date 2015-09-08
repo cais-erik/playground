6bff0f42f5bc4bcddd3bdd1e87e48e037f3425f7
bdavies <bdavies@caisgroup.com>
2015-09-08 10:05:04 -0500

    domination.

--- playground/src/com/cais/playground/HelloWorld.java
+++ playground/src/com/cais/playground/HelloWorld.java
@@ -8,7 +8,7 @@ public class HelloWorld {
 		System.out.println("added with 4th commit from Eclipse (no merge necessary)");
 		System.out.println("Barry added with 5 commit from command line (stash/sphinct/pull/stash-pop)");
 		System.out.println("added from Barry, along with altering another line.");
-		System.out.println("added with 6 commit from Eclipse (no merge necessary)");
+		System.out.println("added with 6 and there is more from Barry, baby, commit from Eclipse (no merge necessary)");
 		System.out.println("added with 7 commit from command line (branch/merge/commit/push)");
 		System.out.println("added from Erik's eclipse");
 		System.out.println("adding another change from Erik's eclipse, no merge necessary");
