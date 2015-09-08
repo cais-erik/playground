8f2c0fc2c8baa0077ea7bc016f0cfbf55772cea6
bdavies <bdavies@caisgroup.com>
2015-09-08 09:43:53 -0500

    made a change that I hope will conflict

--- playground/src/com/cais/playground/HelloWorld.java
+++ playground/src/com/cais/playground/HelloWorld.java
@@ -6,7 +6,8 @@ public class HelloWorld {
 		
 		System.out.println("hello world");
 		System.out.println("added with 4th commit from Eclipse (no merge necessary)");
-		System.out.println("added with 5 commit from command line (stash/pull/stash-pop)");
+		System.out.println("Barry added with 5 commit from command line (stash/sphinct/pull/stash-pop)");
+		System.out.println("added from Barry, along with altering another line.");
 		System.out.println("added with 6 commit from Eclipse (no merge necessary)");
 		System.out.println("added with 7 commit from command line (branch/merge/commit/push)");
 	}
