94f52be1ed7a60fccf81ce7ef5236a39c7beedc1
bdavies <bdavies@caisgroup.com>
2015-09-15 15:15:15 -0500

    testing our SOP

--- playground/src/com/cais/playground/HelloWorld.java
+++ playground/src/com/cais/playground/HelloWorld.java
@@ -3,7 +3,7 @@ package com.cais.playground;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("hello world");
 		System.out.println("added with 4th commit from Eclipse (no merge necessary)");
 		System.out.println("Barry added with 5 commit from command line (stash/sphinct/pull/stash-pop)");
@@ -13,5 +13,6 @@ public class HelloWorld {
 		System.out.println("added from Erik's eclipse");
 		System.out.println("adding another change from Erik's eclipse, no merge necessary");
 		System.out.println("adding yet another change from Erik's eclipse, merge will be necessary");
+		System.out.println("Boots w/ the fuurrrrrr...");
 	}
 }
