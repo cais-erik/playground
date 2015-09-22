05090c521208a34d9357cff581adadcacc572285
bdavies <bdavies@caisgroup.com>
2015-09-22 15:11:24 -0500

    more

--- playground/src/com/cais/playground/Jane.java
+++ playground/src/com/cais/playground/Jane.java
@@ -1,10 +1,16 @@
 package com.cais.playground;
 
+import java.util.Date;
+
 public class Jane {
 
 	// something to commit
 	// ORIG_HEAD test
 	public String getSphinct() {
 		return "Erik's is operating within normal parameters.";
+	}
+
+	public Date getBorn() {
+		return new Date();
 	}
 }
