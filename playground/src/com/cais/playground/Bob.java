1d4b32bcb3570acb86f7d224b8134fe7248f80eb
bdavies <bdavies@caisgroup.com>
2015-09-22 15:23:26 -0500

    Trying again, with explicit merge at the end, tho.

--- playground/src/com/cais/playground/Bob.java
+++ playground/src/com/cais/playground/Bob.java
@@ -2,15 +2,18 @@ package com.cais.playground;
 
 public class Bob {
 
-	public void nervig()
-	{
-		System.out.println( "Difference!" );
-		//seeing if changing (but not yet committing) a file creates orig_head
-		//trying again
-		//Davies strikes again
-		//Orchestrating a conflict.
-		//Orchestrating a conflict again (manually merged)
-		//a change erik will commit and then merge into remote commits
-		//Orchestrating an opera (manually merged)
+	public void nervig() {
+		System.out.println("Difference!");
+		// seeing if changing (but not yet committing) a file creates orig_head
+		// trying again
+		// Davies strikes again
+		// Orchestrating a conflict.
+		// Orchestrating a conflict again (manually merged)
+		// a change erik will commit and then merge into remote commits
+		// Orchestrating an opera (manually merged)
+	}
+
+	public String getBobStuff() {
+		return "What about Bob?";
 	}
 }
