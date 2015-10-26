1d38be64d1e4b5160f4f2b0e7965e0e878020d89
Barry Davies <bdavies@caisgroup.com>
2015-10-26 14:52:59 -0500

    Making Master diverge from our other branches

---
+++ playground/src/com/cais/playground/ElwayRules.java
@@ -0,0 +1,12 @@
+package com.cais.playground;
+
+import java.util.logging.Logger;
+
+public class ElwayRules {
+
+	private Logger log = Logger.getAnonymousLogger();
+
+	public ElwayRules() {
+		log.info("testing");
+	}
+}
