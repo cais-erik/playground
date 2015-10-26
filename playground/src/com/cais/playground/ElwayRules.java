2f4942598317097fef784461f25b146596a4a573
Barry Davies <bdavies@caisgroup.com>
2015-10-26 14:57:35 -0500

    Commit 2 in master while other stuff is branched.

--- playground/src/com/cais/playground/ElwayRules.java
+++ playground/src/com/cais/playground/ElwayRules.java
@@ -7,6 +7,10 @@ public class ElwayRules {
 	private Logger log = Logger.getAnonymousLogger();
 
 	public ElwayRules() {
-		log.info("testing");
+		log.info("Commit 1 while other things branched off");
+	}
+
+	public String getJackwagon() {
+		return "Jack.  And his wagon.  Commit 2 while other things branched off.";
 	}
 }
